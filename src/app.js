6f6e3c07f47fb4d5d06cfac0bf1337d315346884
Pedro <pedropiacentini@usp.br>
2023-04-07 19:55:21 -0300

    post/get

--- src/app.js
+++ src/app.js
@@ -1,5 +1,50 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
-app.listen(5000);
+app.use(cors());
+app.use(express.json());
+
+const users = []
+const tweets = [];
+const lastTweets = [];
+let avatar = "";
+
+let isLogged = false;
+
+function tweetsManage(tweet) {
+    tweets.push(tweet);
+    if (tweets.length < 10) {
+        lastTweets = [...tweets];
+    } else {
+        lastTweets = tweets.slice(-10);
+    }
+}
+
+app.post("/sing-up", (req, res) => {
+    const user = req.body;
+    avatar = user.avatar;
+    users.push(user);
+    isLogged = true;
+    res.send("ok");
+})
+
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body;
+    if (isLogged) {
+        const postTweet = { username, tweet, avatar };
+        tweetsManage(tweet);
+        res.send("ok");
+    } else {
+        res.send("UNAUTHORIZED")
+    }
+})
+
+app.get("/tweets", (req, res) => {
+    res.send(lastTweets);
+})
+
+
+
+app.listen(5000, () => console.log("Rodando servidor na porta 5000"));
